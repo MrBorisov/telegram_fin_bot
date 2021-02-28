28683fbeaaa9f10985e53abe42dcdc286a4991f7
Dmitriy <mrborisovv@gmail.com>
2021-02-28 19:39:43 +1000

    states test

--- bot.py
+++ bot.py
@@ -1,21 +1,72 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-
 from config import TOKEN
 
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
+from messages import MESSAGES
+from states import TestStates
+
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
+dp.middleware.setup(LoggingMiddleware())
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+    await message.reply(MESSAGES['start'])
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+    await message.reply(MESSAGES['help'])
+
+
+@dp.message_handler(state='*', commands=['setstate'])
+async def process_setstate_command(message: types.Message):
+    argument = message.get_args()
+    state = dp.current_state(user=message.from_user.id)
+    if not argument:
+        await state.reset_state()
+        return await message.reply(MESSAGES['state_reset'])
+
+    if (not argument.isdigit()) or (not int(argument) < len(TestStates.all())):
+        return await message.reply(MESSAGES['invalid_key'].format(key=argument))
+
+    await state.set_state(TestStates.all()[int(argument)])
+    await message.reply(MESSAGES['state_change'], reply=False)
+
+
+@dp.message_handler(commands=['for'], state=TestStates.TEST_STATE_0)
+async def second_test_state_case_met(message: types.Message):
+    await message.reply('Команда for!', reply=False)
+
+
+@dp.message_handler(state=TestStates.TEST_STATE_0)
+async def second_test_state_case_met(message: types.Message):
+    await message.reply('Нулевой!', reply=False)
+
+
+@dp.message_handler(state=TestStates.TEST_STATE_1)
+async def first_test_state_case_met(message: types.Message):
+    await message.reply('Первый!', reply=False)
+
+
+@dp.message_handler(state=TestStates.TEST_STATE_2)
+async def second_test_state_case_met(message: types.Message):
+    await message.reply('Второй!', reply=False)
+
+
+@dp.message_handler(state=TestStates.all())
+async def some_test_state_case_met(message: types.Message):
+    with dp.current_state(user=message.from_user.id) as state:
+        text = MESSAGES['current_state'].format(
+            current_state=await state.get_state(),
+            states=TestStates.all()
+        )
+    await message.reply(text, reply=False)
 
 
 @dp.message_handler()
