815dbccc6692f88a92c6bc74ed4f94c2622c54d3
zeus <mrborisovv@gmail.com>
2021-05-28 17:45:49 +1000

    исправил парсер, дополнил базу

--- bd.py
+++ bd.py
@@ -2,3 +2,6 @@ import sqlite3
 
 conn = sqlite3.connect('base.db')
 cur = conn.cursor()
+
+
+
--- parser.py
+++ parser.py
@@ -1,6 +1,17 @@
 def p_message(message):
-    print(message)
+    err = False
     x = message.split('. ')
     category = x[3]
     price = x[2].split(' ')[0]
-    return (category, price)
+    """
+    тут пробуем проверить парсер, если он непавильно сработал, то в переменной price
+    будет не число и мы получем исключение и вернем все что распарсили и true
+    """
+    try:
+        float(price)
+    except ValueError:
+        err = True
+    if err:
+        return category, price, err
+    else:
+        return category, float(price), err
